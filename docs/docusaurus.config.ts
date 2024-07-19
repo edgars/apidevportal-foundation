f82931463064c91759fda2f04ef7603084fb45c1
edgars <edgar@skalena.com>
2024-07-18 21:42:15 -0300

    adding infos

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -11,10 +11,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://qriar-labs.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/apidevportal-foundation',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -43,15 +43,15 @@ const config: Config = {
           docItemComponent: "@theme/ApiItem",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        //editUrl:
+         //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          //editUrl:
+          //  'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -66,8 +66,8 @@ const config: Config = {
     navbar: {
       title: 'QriarLabs API Devportal',
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/just_q_blue.png',
+        alt: 'QriarLabs DevPortal',
+        src: '../img/just_q_blue.png',
       },
       items: [
         {
